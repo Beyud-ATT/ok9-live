754d7a52be31428c561f6772af5a108360f9fb47
BEYUD <beyud@attcloud.org>
2025-04-25 21:17:11 +0700

    build 215/04

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -27,8 +27,8 @@ const UserType = {
 
 const chatHeightSetting = `xl:h-[525px] lg:h-[390px] md:h-[285px]`;
 
-const videoHeightSettingInRoom = `xl:h-[504px] lg:h-[371px] md:h-[265px] h-[200px]`;
-const videoHeightSettingInHome = `xl:h-[575px] lg:h-[430px] md:h-[330px] h-[200px]`;
+const videoHeightSettingInRoom = `xl:h-[488px] lg:h-[371px] md:h-[265px] h-[200px]`;
+const videoHeightSettingInHome = `xl:h-[510px] lg:h-[430px] md:h-[330px] h-[200px]`;
 
 const GIFTS = [
   {
